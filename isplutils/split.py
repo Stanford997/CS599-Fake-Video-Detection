fdcfe813d248715f6812c44c30f3394726690cff
Stanford997 <545255309@qq.com>
2024-05-06 12:32:11 -0400

    add InceptionV3

--- isplutils/split.py
+++ isplutils/split.py
@@ -56,9 +56,12 @@ def get_split_df(df: pd.DataFrame, dataset: str, split: str) -> pd.DataFrame:
         crf = dataset.split('-')[1]
         random_youtube_videos = np.random.permutation(
             df[df['quality'] == crf]['video'].unique())
+        # train_orig = random_youtube_videos[:720]
+        # val_orig = random_youtube_videos[720:720 + 140]
+        # test_orig = random_youtube_videos[720 + 140:]
         train_orig = random_youtube_videos[:2100]
-        val_orig = random_youtube_videos[2100:2100 + 350]
-        test_orig = random_youtube_videos[2100 + 350:]
+        val_orig = random_youtube_videos[2100:2450]
+        test_orig = random_youtube_videos[2450:2800]
         if split == 'train':
             split_df = pd.concat((df[df['original'].isin(train_orig)], df[df['video'].isin(train_orig)]), axis=0)
         elif split == 'val':
